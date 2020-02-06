bf835a5e27a2ec11716170872ff3ee75ecdae95a
Monica Rondón <monirondon15@gmail.com>
2020-02-06 20:37:50 +0100

    Initial Commit

---
+++ ExecutionSequencer/ExecutionSequencer.h
@@ -0,0 +1,19 @@
+//
+//  ExecutionSequencer.h
+//  ExecutionSequencer
+//
+//  Created by Monica Rondón on 2/6/20.
+//  Copyright © 2020 Monica Rondón. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExecutionSequencer.
+FOUNDATION_EXPORT double ExecutionSequencerVersionNumber;
+
+//! Project version string for ExecutionSequencer.
+FOUNDATION_EXPORT const unsigned char ExecutionSequencerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExecutionSequencer/PublicHeader.h>
+
+
